3b109ec5ca2b6b3bbe34073ac4a8b9a4fd0dc866
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-09-25 06:35:48 +0200

    feat(server): connect sqlite db, add typeorm

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,12 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import Joi from 'joi';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServerFeatureExpenseModule } from '@hba/server/feature-expense';
 
 @Module({
-  imports: [ServerFeatureExpenseModule],
+  imports: [
+    ConfigModule.forRoot({
+    isGlobal: true,
+    validationSchema: Joi.object({
+      DATABASE_PATH: Joi.string().default('tmp/db.sqlite'),
+    }),
+  }),
+  TypeOrmModule.forRootAsync({
+    useFactory: (config: ConfigService) => ({
+      type: 'sqlite',
+      database: config.get('DATABASE_PATH'),
+      synchronize: true,
+      logging: true,
+      autoLoadEntities: true,
+    }),
+  }),
+  ServerFeatureExpenseModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
