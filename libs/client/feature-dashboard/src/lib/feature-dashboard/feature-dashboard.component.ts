1274d903effac8198d60474551ea9c37ad69b7d9
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-09-29 17:09:54 +0200

    feat(client): display expenses list in columns

--- libs/client/feature-dashboard/src/lib/feature-dashboard/feature-dashboard.component.ts
+++ libs/client/feature-dashboard/src/lib/feature-dashboard/feature-dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '@hba/client/data-access';
+import { ExpenseComponent } from '@hba/client/ui-components';
 
 @Component({
   selector: 'lib-feature-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ExpenseComponent],
   templateUrl: './feature-dashboard.component.html',
   styleUrl: './feature-dashboard.component.scss',
 })
@@ -13,8 +14,4 @@ export class FeatureDashboardComponent {
   private readonly apiService = inject(ApiService);
 
   expenses$ = this.apiService.getAllExpenses();
-
-  NgOnInit() {
-    console.log('expenses$', this.expenses$);
-  }
 }
