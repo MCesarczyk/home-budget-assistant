6f3bf212e36310d45775800321b1a61bdc716a7d
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2024-10-05 12:01:22 +0200

    feat(build): prepare deployment (#6)
    * feat(libs): create healthckeck lib using terminus
    
    * build: setup docker, prepare for deployment to Render

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -4,27 +4,30 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import Joi from 'joi';
 
 import { ServerFeatureExpenseModule } from '@hba/server/feature-expense';
+import { ServerFeatureHealthModule } from '@hba/server/feature-health';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-    isGlobal: true,
-    validationSchema: Joi.object({
-      DATABASE_PATH: Joi.string().default(process.env.DATABASE_PATH),
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_PATH: Joi.string().default(process.env.DATABASE_PATH),
+      }),
     }),
-  }),
-  TypeOrmModule.forRootAsync({
-    useFactory: () => ({
-      type: 'sqlite',
-      database: process.env.DATABASE_PATH,
-      synchronize: true,
-      logging: true,
-      autoLoadEntities: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'sqlite',
+        database: process.env.DATABASE_PATH,
+        synchronize: true,
+        logging: true,
+        autoLoadEntities: true,
+      }),
     }),
-  }),
-  ServerFeatureExpenseModule],
+    ServerFeatureExpenseModule,
+    ServerFeatureHealthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
