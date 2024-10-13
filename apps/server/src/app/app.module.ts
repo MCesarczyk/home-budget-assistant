39cf93fa028a65356197804c9425d668f218312f
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-10-13 17:27:16 +0200

    build: switch to internal db url on production

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import Joi from 'joi';
 
 import { ServerFeatureExpenseModule } from '@hba/server/feature-expense';
 import { ServerFeatureHealthModule } from '@hba/server/feature-health';
@@ -12,13 +11,6 @@ import { AppService } from './app.service';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      // validationSchema: Joi.object({
-      //   DATABASE_NAME: Joi.string().default(process.env.DATABASE_NAME),
-      //   DATABASE_HOST: Joi.string().default(process.env.DATABASE_HOST),
-      //   DATABASE_PORT: Joi.number().default(process.env.DATABASE_PORT),
-      //   DATABASE_USERNAME: Joi.string().default(process.env.DATABASE_USERNAME),
-      //   DATABASE_PASSWORD: Joi.string().default(process.env.DATABASE_PASSWORD),
-      // }),
     }),
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
@@ -28,6 +20,7 @@ import { AppService } from './app.service';
         port: parseInt(process.env.DATABASE_PORT),
         username: process.env.DATABASE_USERNAME,
         password: process.env.DATABASE_PASSWORD,
+        url: process.env.DATABASE_URL,
         synchronize: true,
         logging: true,
         autoLoadEntities: true,
