9b9b8bbade6c2f0bddf49f2085e789422f272973
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-10-13 19:07:39 +0200

    build: restore explicit db connection

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -15,13 +15,11 @@ import { AppService } from './app.service';
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'postgres',
-        // database: process.env.DATABASE_PATH,
-        // host: process.env.DATABASE_HOST,
-        // port: parseInt(process.env.DATABASE_PORT),
-        // username: process.env.DATABASE_USERNAME,
-        // password: process.env.DATABASE_PASSWORD,
-        url: process.env.DATABASE_URL,
-        ssl: process.env.NODE_ENV === 'production' ? false : true,
+        database: process.env.DATABASE_PATH,
+        host: process.env.DATABASE_HOST,
+        port: parseInt(process.env.DATABASE_PORT),
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
         synchronize: true,
         logging: true,
         autoLoadEntities: true,
